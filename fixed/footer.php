a9d329afbf667612423e5a2ade5020baa2915910
Bruno <1994.brunoalva@gmail.com>
2019-10-23 16:06:47 -0500

    actualizacion de proyecto

--- fixed/footer.php
+++
@@ -1,5 +0,0 @@
-<!--Start footer-->
-<footer class="footer">
-    <span>Copyright &copy; <?php echo date('Y')?>. developed by Luna Systems Peru</span>
-</footer>
-<!--end footer-->
--- modals/m_sin_caja.php
+++
@@ -1,7 +0,0 @@
-<div class="col-md-offset-4 col-md-4 ">
-            <img src="images/ops.jpg" alt="" class="img-responsive ">
-       <div class="panel-body">
-            <h4>Abrir Caja para continuar</h4>
-        </div>
-    </div><!--end panel blog-->
-</div>
