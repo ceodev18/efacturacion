0ce9125f72aa181856e2f21aa0b16a04135101a1
Bruno <1994.brunoalva@gmail.com>
2019-10-23 20:17:17 -0500

    modificacion en el login y registro

--- controller/login.php
+++ controller/login.php
@@ -1,13 +1,17 @@
 <?php
 
-session_start();
+
+//$_SESSION['logeado'] = true;
 
 require '../models/Empresa.php';
+
 
 $c_empresa = new Empresa();
 
 $ruc = filter_input(INPUT_POST, 'ruc_empresa');
 $password = filter_input(INPUT_POST, 'password');
+
+
 
 $c_empresa->setRuc($ruc);
 
@@ -15,10 +19,14 @@ if ($c_empresa->validarLogin()) {
     $c_empresa->obtenerDatos();
     if ($c_empresa->getPassword() == $password) {
         $_SESSION['id_empresa'] = $c_empresa->getIdEmpresa();
+
         header("Location: ../index.php");
     } else {
+        $_SESSION['logeado'] = false;
         header("Location: ../login.php?error=CONTRASEÑA INCORRECTA");
     }
 } else {
     header("Location: ../login.php?error=EMPRESA NO EXISTE");
+    $_SESSION['logeado'] = false;
 }
+
